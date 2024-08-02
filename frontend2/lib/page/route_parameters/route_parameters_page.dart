5313d411806012b48bf8823da88e7b1bfb2fdd9f
Stypox <stypox@pm.me>
2024-08-02 21:51:30 +0200

    Add form to route parameters page

--- frontend2/lib/page/route_parameters/route_parameters_page.dart
+++ frontend2/lib/page/route_parameters/route_parameters_page.dart
@@ -22,19 +22,42 @@ class RouteParametersPageArgs {
 class _RouteParametersPageState extends State<RouteParametersPage>
     with GetItStateMixin<RouteParametersPage> {
   final LatLng? source = null; // null means "current position"
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
     final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(l10n.planYourRoute)
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-
+      appBar: AppBar(title: Text(l10n.planYourRoute)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // TextFormField(
+                //   decoration: InputDecoration(labelText: l10n.routeSource),
+                //   validator: (value) {
+                //     if (value?.isEmpty ?? true) {
+                //       return l10n.insertPassword;
+                //     } else {
+                //       return null;
+                //     }
+                //   },
+                //   onSaved: (value) => password = value,
+                //   keyboardType: TextInputType.visiblePassword,
+                //   obscureText: true,
+                //   autofillHints: const [AutofillHints.password],
+                //   textInputAction: TextInputAction.done,
+                //   onFieldSubmitted: (_) => submitForm(),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
