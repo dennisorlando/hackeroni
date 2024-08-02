c94a9c8542254e20bf5e00971a937d8a43fd3d09
Stypox <stypox@pm.me>
2024-08-02 19:28:25 +0200

    Fix search bar position

--- frontend2/lib/page/map/search_bar.dart
+++ frontend2/lib/page/map/search_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get_it_mixin/get_it_mixin.dart';
 import 'package:insigno_frontend/networking/backend.dart';
 import 'package:insigno_frontend/networking/data/osm_nominatim_entry.dart';
-import 'package:get_it_mixin/get_it_mixin.dart';
 
 /// Flutter code sample for [SearchBar].
 
@@ -17,56 +17,63 @@ class _SearchBarAppState extends State<SearchBarApp> with GetItStateMixin<Search
 
   @override
   Widget build(BuildContext context) {
-    return SearchAnchor(
-        builder: (BuildContext context, SearchController controller) {
-      return SearchBar(
-        controller: controller,
-        padding: const WidgetStatePropertyAll<EdgeInsets>(
-            EdgeInsets.symmetric(horizontal: 16.0)),
-        onTap: () {
-          controller.openView();
-        },
-        onChanged: (_) {
-          controller.openView();
-        },
-        leading: const Icon(Icons.search),
-        trailing: <Widget>[
-          Tooltip(
-            message: 'Change brightness mode',
-            child: IconButton(
-              isSelected: isDark,
-              onPressed: () {
-                setState(() {
-                  isDark = !isDark;
-                });
-              },
-              icon: const Icon(Icons.wb_sunny_outlined),
-              selectedIcon: const Icon(Icons.brightness_2_outlined),
-            ),
-          )
-        ],
-      );
-    }, suggestionsBuilder: (BuildContext context, SearchController controller) async {
-          List<OsmNominatimEntry> entries = await get<Backend>().loadNominatimEntries(controller.text);
-      return List<ListTile>.generate(entries.length, (int index) {
-        final String item = entries[index].displayName;
-        return ListTile(
-          title: Text(item),
+    final mediaQuery = MediaQuery.of(context);
+
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        8 + mediaQuery.padding.left,
+        8 + mediaQuery.padding.top,
+        8 + mediaQuery.padding.right,
+        0,
+      ),
+      child: SearchAnchor(builder: (BuildContext context, SearchController controller) {
+        return SearchBar(
+          controller: controller,
+          padding: const WidgetStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: 16.0)),
           onTap: () {
-            setState(() {
-              controller.closeView(item);
-            });
+            controller.openView();
           },
+          onChanged: (_) {
+            controller.openView();
+          },
+          leading: const Icon(Icons.search),
+          // trailing: <Widget>[
+          //   Tooltip(
+          //     message: 'Change brightness mode',
+          //     child: IconButton(
+          //       isSelected: isDark,
+          //       onPressed: () {
+          //         setState(() {
+          //           isDark = !isDark;
+          //         });
+          //       },
+          //       icon: const Icon(Icons.wb_sunny_outlined),
+          //       selectedIcon: const Icon(Icons.brightness_2_outlined),
+          //     ),
+          //   )
+          // ],
         );
-      });
-    });
+      }, suggestionsBuilder: (BuildContext context, SearchController controller) async {
+        List<OsmNominatimEntry> entries =
+            await get<Backend>().loadNominatimEntries(controller.text);
+        return List<ListTile>.generate(entries.length, (int index) {
+          final String item = entries[index].displayName;
+          return ListTile(
+            title: Text(item),
+            onTap: () {
+              setState(() {
+                controller.closeView(item);
+              });
+            },
+          );
+        });
+      }),
+    );
   }
 }
 
 List<ListTile> searchBarSuggestionBuilder() {
   List<ListTile> list = List<ListTile>.empty(growable: true);
-
-
 
   return list;
 }
